e5b99e255a2de57b1552b31aab3c509757e29e13
Oscar Megía López <pfgytk@gmail.com>
2020-02-25 10:49:55 +0100

    Ejercicio 13: Vamos a crear un componente nuestro denominado Fotos (Widget).
    Para ello vamos a crear una carpeta denominada components y crear un fichero denominado Foto.

---
+++ components/Foto.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace app\components;
+
+use yii\base\Widget;
+use yii\helpers\Html;
+
+class Foto extends Widget
+{
+    public $nombre = "nada.png";
+    public $alternativo = "nada";
+    
+    public function init() {
+        parent::init();
+    }
+    
+    public function run() {
+        return Html::img("@web/imgs/$this->nombre", ["alt" => $this->alternativo]);
+    }
+}
